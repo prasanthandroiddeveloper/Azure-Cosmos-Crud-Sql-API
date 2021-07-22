89074391df3920525412f59d2b4d6b1dac958299
Prasanth Sanjeevi <prasanths@sssbi.com>
2021-07-22 17:38:36 +0530

    key removed api

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  endpoint: "https://azurecosmoskapturas.documents.azure.com:443/",
-  key: "E6IHZTLqEf8eaIgqrweRDXRcQXHWinRIoCMaD7Gyqr854S5yxzXXgRu3Fo6M4gP94kKQgRPUHp9l7J5RQ5wuyg==",
+  endpoint: "",
+  key: "",
   databaseId: "Tasks",
   containerId: "Sourcedata",
   partitionKey: { kind: "Hash" },
